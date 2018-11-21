9c8a6226eee704d60db66334dd954fd65b12c287
Chris Hassan <chriskkhassan@gmail.com>
2018-11-21 18:08:24 +0000

    returns buzz if num divisible by 5

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -1,6 +1,8 @@
 def fizzbuzz(num)
   if num % 3 == 0
     return 'fizz'
+  elsif num % 5 ==0
+    return "buzz"
   else
     return num
   end
--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -8,4 +8,8 @@ describe 'Fizzbuzz' do
   it "returns the number if not divisible by 3" do
     expect(fizzbuzz(4)).to eq 4
   end
+
+  it "returns buzz if the number is dvisible by 5" do
+    expect(fizzbuzz(5)).to eq "buzz"
+  end
 end
