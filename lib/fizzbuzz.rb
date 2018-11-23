c66bf968a036bf5b9caa96296937640f7aca0dec
Chris Hassan <chriskkhassan@gmail.com>
2018-11-23 14:32:56 +0000

    refactored fizzbuzz method

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -1,13 +1,8 @@
 def fizzbuzz(num)
-  if num % 15 == 0
-    return "fizzbuzz"
-  elsif num % 3 == 0
-    return 'fizz'
-  elsif num % 5 ==0
-    return "buzz"
-  else
-    return num
-  end
+  return "fizzbuzz" if num % 15 == 0
+  return "fizz" if num % 3 == 0
+  return "buzz" if num % 5 ==0
+  num
 end
 
 (1..100).each do |num|
